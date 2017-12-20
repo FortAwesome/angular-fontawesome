2db38f86a4f515cc103e48e1a932a70ee5453a37
Mike Wilkerson <mwilkerson@gmail.com>
2017-12-20 07:50:35 -0800

    fix welcome message expectation

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angular-fontawesome App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Font Awesome 5 Angular Demo');
   });
 });
