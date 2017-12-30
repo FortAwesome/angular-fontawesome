123544467a1fe77b6c28e617c1d75fedc29b25ea
Mike Wilkerson <mwilkerson@gmail.com>
2017-12-30 06:57:43 -0800

    deleting these files that were used temporarily. we now have real type definitions in the relevant packages.

--- src/lib/fortawesome__fontawesome-free-solid.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@fortawesome/fontawesome-free-solid' {
-  import { Icon } from '@fortawesome/fontawesome';
-  export const faCoffee: Icon;
-  export const faSquare: Icon;
-  export const faSync: Icon;
-  export const faCircle: Icon;
-}
