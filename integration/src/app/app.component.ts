b7270539859ee2101f9fd0992c9b63c9ad4359b9
Mike Wilkerson <mwilkerson@gmail.com>
2018-01-02 13:04:30 -0800

    use the refactored FontawesomeService

--- integration/src/app/app.component.ts
+++ integration/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { LibService } from 'quickstart-lib';
+import { FontawesomeService } from 'quickstart-lib';
 
 @Component({
   selector: 'integration-app',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  fontawesome: any;
-  constructor(libService: LibService) {
-   this.fontawesome = libService.fontawesome;
-  }
+  constructor(private fontawesome: FontawesomeService) {}
 }
