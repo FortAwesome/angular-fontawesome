29ff95fa534387aa53076cc500b294271e49c5f6
Mike Wilkerson <mwilkerson@gmail.com>
2018-02-21 11:57:59 -0800

    Improve Examples
    - Add example showing how to specify an array with a non-default prefix
    and iconName.
    - Add further explanation of the examples.

--- examples/example.component.ts
+++ examples/example.component.ts
@@ -6,6 +6,7 @@ import { faUser,
   faCircle,
   faSquare,
   faSync } from '@fortawesome/fontawesome-free-solid';
+import { faUser as regularUser } from '@fortawesome/fontawesome-free-regular';
 import { library } from '@fortawesome/fontawesome';
 
 @Component({
@@ -14,16 +15,36 @@ import { library } from '@fortawesome/fontawesome';
   styleUrls: []
 })
 export class ExampleComponent {
-  faUser = faUser;
+  faCoffee = faCoffee;
   faMagic = faMagic;
   faAdjust = faAdjust;
   faCircle = faCircle;
   faSquare = faSquare;
   faSync = faSync;
+  regularUser = regularUser;
   isSyncAnimated = true;
   magicLevel = 0;
 
   constructor() {
-    library.add(faCoffee);
+    // Notice that we're adding two different icon objects to the library.
+    // Each of them within their respective icon npm packages are exported as faUser,
+    // but we've renamed the second one in order to disambiguate the two objects within 
+    // this JavaScript module. Internally, these objects are different, even though they have the same iconName. 
+    // They have different prefixes: faUser has a prefix of fas, since it came from fontawesome-free-solid;
+    // regularUser has a prefix of far, since it came from fontawesome-free-regular.
+    // And of course, they also have different SVG content, resulting in different appearances.
+    // So they really are totally different icons. However, they share the same iconName: user.
+    // So in the template, the only way to reference the non-default (fas) icon is to either
+    // use the array syntax that specifies [prefix, iconName], like this:
+    // 
+    // <fa-icon [icon]="['far','user']"></fa-icon>
+    //
+    // Or we could make the regularUser object available to the template and simply
+    // reference it as an object, like this: 
+    //
+    // <fa-icon [icon]="regularUser"></fa-icon>
+    //
+    // You don't specify the prefix in that case, because the icon object knows its own prefix.
+    library.add(faUser, regularUser);
   }
 }
