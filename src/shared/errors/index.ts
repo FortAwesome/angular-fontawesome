d2ba61550dcd7e74d895d55eda9da0d00558c529
Zeev Katz <zeev@proteantecs.com>
2018-03-03 01:49:31 +0200

    adding warn method to detect non-existing parent components

--- src/shared/errors/index.ts
+++ src/shared/errors/index.ts
@@ -1,3 +1,4 @@
 export * from './not-found-icon-html';
-export * from './throw-if-icon-html-missing';
-export * from './throw-if-icon-spec-missing';
+export * from './warn-if-parent-not-exist';
+export * from './warn-if-icon-html-missing';
+export * from './warn-if-icon-spec-missing';
