a8a4cc80868716bfb852c2cf073b522355248785
Zeev Katz <zeev@proteantecs.com>
2018-03-03 01:29:30 +0200

    renamed from throw to warn

--- src/shared/errors/warn-if-icon-html-missing.ts
+++ src/shared/errors/warn-if-icon-html-missing.ts
@@ -1,11 +1,11 @@
 import { Icon, IconLookup } from '@fortawesome/fontawesome';
 
 /**
- * Throws error if icon html missing although there is icon spec.
+ * Warns if icon html missing although there is icon spec.
  * @param {Icon} iconObj
  * @param {IconLookup} iconSpec
  */
-export const faThrowIfIconHtmlMissing = (iconObj: Icon, iconSpec: IconLookup) => {
+export const faWarnIfIconHtmlMissing = (iconObj: Icon, iconSpec: IconLookup) => {
   if (iconSpec && !iconObj) {
     console.error(`FontAwesome: Could not find icon with iconName=${iconSpec.iconName} and prefix=${iconSpec.prefix}`);
   }
--- src/shared/errors/warn-if-icon-spec-missing.ts
+++ src/shared/errors/warn-if-icon-spec-missing.ts
@@ -1,10 +1,10 @@
 import { IconLookup } from '@fortawesome/fontawesome';
 
 /**
- * Throws if icon spec missing.
+ * Warns if icon spec missing.
  * @param {IconLookup} iconSpec
  */
-export const faThrowIfIconSpecMissing = (iconSpec: IconLookup) => {
+export const faWarnIfIconSpecMissing = (iconSpec: IconLookup) => {
   if (!iconSpec) {
     console.error('FontAwesome: Could not find icon. ' +
       `It looks like you've provided a null or undefined icon object to this component.`);
