f91f978c879c077bc4a10bfb5bffea4e2484a46e
Zeev Katz <zeev@proteantecs.com>
2018-03-16 19:32:13 +0200

    fa-layers component improved and simplified

--- src/layers/layers.component.ts
+++ src/layers/layers.component.ts
@@ -1,48 +1,15 @@
-import {
-  Input,
-  Component,
-  OnChanges,
-  OnDestroy,
-  QueryList,
-  HostBinding,
-  SimpleChanges,
-  ContentChildren
-} from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { Layer, Text, Icon } from '@fortawesome/fontawesome';
-import { faNotFoundIconHtml } from '../shared/errors';
-import { FaIconComponent } from '../icon';
-import { FaLayersTextComponent } from './layers-text.component';
+import { Component, HostBinding } from '@angular/core';
 
 /**
  * Fontawesome layers.
  */
 @Component({
   selector: 'fa-layers',
-  template: `<span class="fa-layers">
-    <ng-template ngFor let-child [ngForOf]="iconsChildren">
-      <fa-icon [icon]="child.icon" [styles]="child.styles" [transform]="child.transform" [inverse]="child.inverse"></fa-icon>
-    </ng-template>
-    <ng-template ngFor let-child [ngForOf]="textsChildren">
-      <fa-layers-text [content]="child.content" [transform]="child.transform" [styles]="child.styles" [counter]="child.counter"></fa-layers-text>
-    </ng-template>
-  </span>`,
+  template: `<ng-content select="fa-icon, fa-layers-text"></ng-content>`,
     styleUrls: ['layers.component.scss'],
 })
 export class FaLayersComponent {
-  constructor(private sanitizer: DomSanitizer) {}
-
-  @ContentChildren(FaIconComponent)
-  private iconsChildren: QueryList<FaIconComponent>;
-
-  @ContentChildren(FaLayersTextComponent)
-  private textsChildren: QueryList<FaLayersTextComponent>;
-
-  private _classes?: string[] = [];
-  @Input()
-  get classes() { return this._classes; }
-  set classes(classes: string[]) {
-    this._classes = (classes instanceof Array) ? classes : [];
-  }
+  @HostBinding('class.fa-layers')
+  private cssClass = true;
 }
 
