74a1fdeff46dd09f95b7fcfb7a684d046ed8d49c
Scott Cooper <scttcper@gmail.com>
2018-06-21 14:23:23 -0700

    chore: call chrome in headless for travis

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -26,6 +26,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeCI: {
+        base: `${process.env['TRAVIS'] ? 'ChromeHeadless' : 'Chrome'}`,
+        flags: process.env['TRAVIS'] ? ['--no-sandbox'] : [],
+      },
+    },
     singleRun: false
   });
 };
