bd2cebbed8ec5f6f3712c71967289e586a0ad355
Austin Turner <paustint@gmail.com>
2018-08-10 20:49:01 -0600

    Add size and fixedWidth attribute to fa-layers #79
    Added size and fixedWidth attribute to FaLayersComponent
    moved the {host: class: 'fa-layers'} to a @HostBinding decorator to allow dynamic updates
    Added new class list helper  to build class names
    
    Added unit tests
    Added examples
    Updated gitignore because it was not properly omitting .vscode directory
    
    resolves #79

--- src/lib/layers/layers.component.spec.ts
+++ src/lib/layers/layers.component.spec.ts
@@ -1,7 +1,7 @@
-import { Component, Type } from '@angular/core';
+import { Component, Type, ElementRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { faUser, faMobile } from '@fortawesome/free-solid-svg-icons';
-
+import { By } from '@angular/platform-browser';
 import { FaIconComponent } from '../icon/icon.component';
 import { FaLayersComponent } from './layers.component';
 import { FaLayersTextComponent } from './layers-text.component';
@@ -15,6 +15,10 @@ function initTest<T>(component: Type<T>): ComponentFixture<T> {
 
 function svgLayers(fixture: ComponentFixture<any>): SVGElement {
   return fixture.debugElement.nativeElement.querySelector('svg');
+}
+
+function queryByCss(fixture: ComponentFixture<any>, className: string): ElementRef {
+  return fixture.debugElement.query(By.css(className));
 }
 
 describe('FaLayersCompoennt', () => {
@@ -37,4 +41,65 @@ describe('FaLayersCompoennt', () => {
     fixture.detectChanges();
     expect(svgLayers(fixture)).toBeTruthy();
   });
+
+  it('should include size class', () => {
+    @Component({
+      selector: 'fa-host',
+      template: `
+        <fa-layers size="2x">
+          <fa-icon [icon]="faUser"></fa-icon>
+          <fa-icon [icon]="faMobile"></fa-icon>
+          <fa-layers-text [content]="'User with mobile'" [styles]="{ color: 'Tomato' }"></fa-layers-text>
+        </fa-layers>`
+    })
+    class HostComponent {
+      faUser = faUser;
+      faMobile = faMobile;
+    }
+
+    const fixture = initTest(HostComponent);
+    fixture.detectChanges();
+    expect(queryByCss(fixture, '.fa-2x')).toBeTruthy();
+  });
+
+  it('should include fixed width', () => {
+    @Component({
+      selector: 'fa-host',
+      template: `
+        <fa-layers [fixedWidth]="true">
+          <fa-icon [icon]="faUser"></fa-icon>
+          <fa-icon [icon]="faMobile"></fa-icon>
+          <fa-layers-text [content]="'User with mobile'" [styles]="{ color: 'Tomato' }"></fa-layers-text>
+        </fa-layers>`
+    })
+    class HostComponent {
+      faUser = faUser;
+      faMobile = faMobile;
+    }
+
+    const fixture = initTest(HostComponent);
+    fixture.detectChanges();
+    expect(queryByCss(fixture, '.fa-fw')).toBeTruthy();
+  });
+
+
+  it('should not include fixed width', () => {
+    @Component({
+      selector: 'fa-host',
+      template: `
+        <fa-layers [fixedWidth]="false">
+          <fa-icon [icon]="faUser"></fa-icon>
+          <fa-icon [icon]="faMobile"></fa-icon>
+          <fa-layers-text [content]="'User with mobile'" [styles]="{ color: 'Tomato' }"></fa-layers-text>
+        </fa-layers>`
+    })
+    class HostComponent {
+      faUser = faUser;
+      faMobile = faMobile;
+    }
+    const fixture = initTest(HostComponent);
+    fixture.detectChanges();
+    expect(queryByCss(fixture, '.fa-fw')).toBeFalsy();
+  });
 });
+
--- src/lib/layers/layers.component.ts
+++ src/lib/layers/layers.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges, OnChanges, OnInit, HostBinding } from '@angular/core';
+import { SizeProp } from '@fortawesome/fontawesome-svg-core';
+import { faLayerClassList } from '../shared/utils/classlist.util';
+import { FaProps } from '../public_api';
 
 /**
  * Fontawesome layers.
@@ -6,10 +9,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'fa-layers',
   template: `<ng-content select="fa-icon, fa-layers-text, fa-layers-counter"></ng-content>`,
-  host: {
-    class: 'fa-layers'
-  }
 })
-export class FaLayersComponent {
-}
+export class FaLayersComponent implements OnInit, OnChanges {
+  @Input() size?: SizeProp;
+  @Input() fixedWidth?: boolean;
 
+  @HostBinding('class')
+  hostClass = 'fa-layers';
+
+  ngOnInit() {
+    this.updateClasses();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes) {
+      this.updateClasses();
+    }
+  }
+
+  updateClasses() {
+    const classOpts: FaProps = {
+      size: this.size || null,
+      fixedWidth: this.fixedWidth,
+    };
+    this.hostClass = `fa-layers ${faLayerClassList(classOpts).join(' ')}`;
+  }
+
+}
