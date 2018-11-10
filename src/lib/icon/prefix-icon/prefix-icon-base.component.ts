52896197a00879dc21f84627a2e94394ff17ef12
Zeev Katz <zeev@proteantecs.com>
2018-11-10 20:36:27 +0200

    Fix: cannot determine the module for class FaPrefixIconBaseComponent

--- src/lib/icon/prefix-icon/prefix-icon-base.component.ts
+++ src/lib/icon/prefix-icon/prefix-icon-base.component.ts
@@ -1,4 +1,4 @@
-import { Input } from '@angular/core';
+import { Injectable, Input } from '@angular/core';
 import { icon, IconPrefix, IconName } from '@fortawesome/fontawesome-svg-core';
 
 import { FaIconComponent } from '../icon.component';
@@ -6,12 +6,13 @@ import { FaIconComponent } from '../icon.component';
 /**
  * Fontawesome prefix icon base.
  */
+@Injectable()
 export abstract class FaPrefixIconBaseComponent extends FaIconComponent {
   /**
    * Icon prefix.
    */
   protected readonly abstract iconPrefix: IconPrefix;
-  
+
   /**
    * Overrides the icon input with the defined icon prefix and provided icon name.
    * @param {IconName} iconName
