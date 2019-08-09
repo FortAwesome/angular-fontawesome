00e6221d293374ea5c9c11e63a1e275c3280789e
Yaroslav Admin <devoto13@gmail.com>
2019-08-09 13:41:31 +0200

    Added test for AsyncPipe and warning that missing icon will become an error
    Closes #50

--- src/lib/shared/errors/warn-if-icon-spec-missing.ts
+++ src/lib/shared/errors/warn-if-icon-spec-missing.ts
@@ -1,8 +1,4 @@
-import { IconLookup } from '@fortawesome/fontawesome-svg-core';
-
-export const faWarnIfIconSpecMissing = (iconSpec: IconLookup) => {
-  if (!iconSpec) {
-    console.error('FontAwesome: Could not find icon. ' +
-      `It looks like you've provided a null or undefined icon object to this component.`);
-  }
+export const faWarnIfIconSpecMissing = () => {
+  console.error('FontAwesome: Property `icon` is required for `fa-icon`/`fa-duotone-icon` components. ' +
+    `This warning will become a hard error in 0.6.0.`);
 };
