170922993b77c0744266510a0fa737795cf59f71
Yaroslav Admin <devoto13@gmail.com>
2019-11-10 20:56:13 +0100

    Remove Service suffix to align name with the regular version

--- testing/src/testing.module.ts
+++ testing/src/testing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { MockFaIconLibraryService } from './icon/mock-icon-library.service';
+import { MockFaIconLibrary } from './icon/mock-icon-library.service';
 
 @NgModule({
   exports: [FontAwesomeModule],
-  providers: [{ provide: FaIconLibrary, useExisting: MockFaIconLibraryService }],
+  providers: [{ provide: FaIconLibrary, useExisting: MockFaIconLibrary }],
 })
 export class FontAwesomeTestingModule {}
