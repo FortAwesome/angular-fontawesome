8f40f9d9be50ee372267d9544867cd7562da6d03
Yaroslav Admin <devoto13@gmail.com>
2021-05-13 17:03:49 +0200

    Update FA packages to latest

--- projects/schematics/src/ng-add/versions.ts
+++ projects/schematics/src/ng-add/versions.ts
@@ -1,3 +1,3 @@
-export const svgCoreVersion = '^1.2.34';
-export const angularFontawesomeVersion = '~0.8.2';
-export const iconPackVersion = '^5.15.2';
+export const svgCoreVersion = '^1.2.35';
+export const angularFontawesomeVersion = '~0.9.0';
+export const iconPackVersion = '^5.15.3';
