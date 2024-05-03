aaf9bb91f38230e541e45d9995f278620539ff09
bleistift2 <bleistift@o2mail.de>
2024-05-03 14:04:18 +0200

    Feat: Allow Configuring the testing module to not throw errors when adding icons
    The FontAwesomeTestingModule is now configurable via a `forRoot` method.
    The configuration object passed to it configures how the mock icon library
    should behave when adding icons to it: ignore them, warn about them or throw
    an error.
    
    Fix #440
    https://github.com/FortAwesome/angular-fontawesome/issues/440

--- testing/src/testing.module.ts
+++ testing/src/testing.module.ts
@@ -1,9 +1,34 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MockFaIconLibrary } from './icon/mock-icon-library.service';
+import {
+  FontAwesomeTestingModuleConfig,
+  FontAwesomeTestingModuleConfigInjectionToken,
+  _getFontAwesomeTestingModuleInternalConfig,
+} from './TestingModuleConfig';
 
 @NgModule({
   exports: [FontAwesomeModule],
   providers: [{ provide: FaIconLibrary, useExisting: MockFaIconLibrary }],
 })
-export class FontAwesomeTestingModule {}
+export class FontAwesomeTestingModule {
+  /**
+   * Use this method to configure the module’s behaviour when trying to add icons
+   * and icon packs to the mock icon library.
+   */
+  public static forRoot(config: FontAwesomeTestingModuleConfig = {}): ModuleWithProviders<FontAwesomeTestingModule> {
+    return {
+      ngModule: FontAwesomeTestingModule,
+      providers: [
+        {
+          provide: FaIconLibrary,
+          useExisting: MockFaIconLibrary,
+        },
+        {
+          provide: FontAwesomeTestingModuleConfigInjectionToken,
+          useValue: _getFontAwesomeTestingModuleInternalConfig(config),
+        }
+      ]
+    }
+  }
+}
--- testing/test/integration/module-test.spec.ts
+++ testing/test/integration/module-test.spec.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { ADD_ICON_MESSAGE } from 'testing/src/icon/mock-icon-library.service';
 import { FontAwesomeTestingModule } from 'testing/src/public_api';
 
 @Component({
@@ -11,33 +12,192 @@ import { FontAwesomeTestingModule } from 'testing/src/public_api';
 class HostComponent {}
 
 describe('Using the `FontAwesomeTestingModule', () => {
-  let component: HostComponent;
-  let fixture: ComponentFixture<HostComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [FontAwesomeTestingModule],
-      declarations: [HostComponent],
+  describe('Providing no configuration', () => {
+    // This describe block asserts that the behaviour of versions <= 0.14.1 is maintained
+
+    let component: HostComponent;
+    let fixture: ComponentFixture<HostComponent>;
+  
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [FontAwesomeTestingModule],
+        declarations: [HostComponent],
+      });
+      
+      fixture = TestBed.createComponent(HostComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
     });
-  });
+  
+    it('should allow you to import the module without errors', () => {
+      expect(component).toBeTruthy();
+    });
+  
+    it('should throw on attempt to add an icon to the mocked icon library', () => {
+      const service = TestBed.inject(FaIconLibrary);
+      expect(() => service.addIcons(faUser)).toThrow(new Error(ADD_ICON_MESSAGE));
+    });
+  
+    it('should throw on attempt to add an icon pack to the mocked icon library', () => {
+      const service = TestBed.inject(FaIconLibrary);
+      expect(() => service.addIcons(faUser)).toThrow(new Error(ADD_ICON_MESSAGE));
+    });
+  })
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HostComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  describe('Providing an empty configuration object', () => {
+    // This describe block asserts that a partial configuration object
+    // is correctly filled up to the ‘full’ internal object.
+    // The used configuration should mimick the default values for ‘no configuration’.
 
-  it('should allow you to import the module without errors', () => {
-    expect(component).toBeTruthy();
-  });
+    let component: HostComponent;
+    let fixture: ComponentFixture<HostComponent>;
+  
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [FontAwesomeTestingModule.forRoot({})],
+        declarations: [HostComponent],
+      });
+    });
+  
+    beforeEach(() => {
+      fixture = TestBed.createComponent(HostComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  
+    it('should allow you to import the module without errors', () => {
+      expect(component).toBeTruthy();
+    });
+  
+    it('should throw on attempt to add an icon to the mocked icon library', () => {
+      const service = TestBed.inject(FaIconLibrary);
+      expect(() => service.addIcons(faUser)).toThrow(new Error(ADD_ICON_MESSAGE));
+    });
+  
+    it('should throw on attempt to add an icon pack to the mocked icon library', () => {
+      const service = TestBed.inject(FaIconLibrary);
+      expect(() => service.addIcons(faUser)).toThrow(new Error(ADD_ICON_MESSAGE));
+    });
+  })
 
-  it('should throw on attempt to add an icon to the mocked icon library', () => {
-    const service = TestBed.inject(FaIconLibrary);
-    expect(() => service.addIcons(faUser)).toThrow(new Error('Attempt to add an icon to the MockFaIconLibrary.'));
-  });
 
-  it('should throw on attempt to add an icon pack to the mocked icon library', () => {
-    const service = TestBed.inject(FaIconLibrary);
-    expect(() => service.addIcons(faUser)).toThrow(new Error('Attempt to add an icon to the MockFaIconLibrary.'));
-  });
+  describe('Providing {addIcons: "throwError"}', () => {
+    // This describe block asserts that feature request
+    // https://github.com/FortAwesome/angular-fontawesome/issues/440
+    // is implemented correctly.
+
+    let component: HostComponent;
+    let fixture: ComponentFixture<HostComponent>;
+  
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [FontAwesomeTestingModule.forRoot({whenAddingIcons: 'throwError'})],
+        declarations: [HostComponent],
+      });
+    });
+  
+    beforeEach(() => {
+      fixture = TestBed.createComponent(HostComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  
+    it('should allow you to import the module without errors', () => {
+      expect(component).toBeTruthy();
+    });
+  
+    it('should throw on attempt to add an icon to the mocked icon library', () => {
+      const service = TestBed.inject(FaIconLibrary);
+      expect(() => service.addIcons(faUser)).toThrow(new Error('Attempt to add an icon to the MockFaIconLibrary.'));
+    });
+  
+    it('should throw on attempt to add an icon pack to the mocked icon library', () => {
+      const service = TestBed.inject(FaIconLibrary);
+      expect(() => service.addIcons(faUser)).toThrow(new Error('Attempt to add an icon to the MockFaIconLibrary.'));
+    });
+  })
+
+
+  describe('Providing {addIcons: "logWarning"}', () => {
+    // This describe block asserts that feature request
+    // https://github.com/FortAwesome/angular-fontawesome/issues/440
+    // is implemented correctly.
+
+    let component: HostComponent;
+    let fixture: ComponentFixture<HostComponent>;
+  
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [FontAwesomeTestingModule.forRoot({whenAddingIcons: 'logWarning'})],
+        declarations: [HostComponent],
+      });
+    });
+  
+    beforeEach(() => {
+      fixture = TestBed.createComponent(HostComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  
+    it('should allow you to import the module without errors', () => {
+      expect(component).toBeTruthy();
+    });
+  
+    it('should call console.warn on attempt to add an icon to the mocked icon library', () => {
+      const service = TestBed.inject(FaIconLibrary);
+      spyOn(console, 'warn')
+      expect(() => service.addIcons(faUser)).not.toThrow();
+      expect(console.warn).toHaveBeenCalledOnceWith(ADD_ICON_MESSAGE)
+    });
+  
+    it('should call console.warn on attempt to add an icon pack to the mocked icon library', () => {
+      const service = TestBed.inject(FaIconLibrary);
+      spyOn(console, 'warn')
+      expect(() => service.addIcons(faUser)).not.toThrow();
+      expect(console.warn).toHaveBeenCalledOnceWith(ADD_ICON_MESSAGE)
+    });
+  })
+
+
+
+  describe('Providing {addIcons: "noop"}', () => {
+    // This describe block asserts that feature request
+    // https://github.com/FortAwesome/angular-fontawesome/issues/440
+    // is implemented correctly.
+
+    let component: HostComponent;
+    let fixture: ComponentFixture<HostComponent>;
+  
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [FontAwesomeTestingModule.forRoot({whenAddingIcons: 'noop'})],
+        declarations: [HostComponent],
+      });
+    });
+  
+    beforeEach(() => {
+      fixture = TestBed.createComponent(HostComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  
+    it('should allow you to import the module without errors', () => {
+      expect(component).toBeTruthy();
+    });
+  
+    it('should ignore attempts to add an icon to the mocked icon library', () => {
+      const service = TestBed.inject(FaIconLibrary);
+      spyOn(console, 'warn')
+      expect(() => service.addIcons(faUser)).not.toThrow();
+      expect(console.warn).not.toHaveBeenCalledOnceWith(ADD_ICON_MESSAGE)
+    });
+  
+    it('should ignore attempts to add an icon pack to the mocked icon library', () => {
+      const service = TestBed.inject(FaIconLibrary);
+      spyOn(console, 'warn')
+      expect(() => service.addIcons(faUser)).not.toThrow();
+      expect(console.warn).not.toHaveBeenCalledOnceWith(ADD_ICON_MESSAGE)
+    });
+  })
 });
