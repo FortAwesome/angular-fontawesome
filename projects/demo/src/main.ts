ddd5f20fcf3c746c319688fd44a43b6a01a31de9
Yaroslav Admin <devoto13@gmail.com>
2024-05-10 18:30:27 +0200

    Add SSR to the demo project

--- projects/demo/src/main.ts
+++ projects/demo/src/main.ts
@@ -1,4 +1,6 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideClientHydration()],
+}).catch((err) => console.error(err));
