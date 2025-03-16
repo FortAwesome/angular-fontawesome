9d306ae77b0818b606541854b3108fb687264d8b
Pankaj Parkar <parkarpankaj3@gmail.com>
2025-03-16 23:45:27 +0800

    ngOnChanges to computed

--- src/lib/layers/layers.component.ts
+++ src/lib/layers/layers.component.ts
@@ -1,15 +1,5 @@
 import { DOCUMENT } from '@angular/common';
-import {
-  Component,
-  ElementRef,
-  inject,
-  OnChanges,
-  OnInit,
-  Renderer2,
-  SimpleChanges,
-  input,
-  computed,
-} from '@angular/core';
+import { Component, inject, OnInit, input, computed } from '@angular/core';
 import { SizeProp } from '@fortawesome/fontawesome-svg-core';
 import { FaConfig } from '../config';
 import { ensureCss } from '../shared/utils/css';
@@ -21,34 +11,30 @@ import { ensureCss } from '../shared/utils/css';
   selector: 'fa-layers',
   template: `<ng-content></ng-content>`,
   host: {
-    '[class.fa-fw]': 'faFw()',
+    '[class]': 'classes()',
   },
 })
-export class FaLayersComponent implements OnInit, OnChanges {
+export class FaLayersComponent implements OnInit {
   readonly size = input<SizeProp>();
   readonly fixedWidth = input<boolean>();
   readonly faFw = computed(() => {
     const fixedWidth = this.fixedWidth();
     return typeof fixedWidth === 'boolean' ? fixedWidth : this.config.fixedWidth;
   });
+  readonly classes = computed(() => {
+    const sizeValue = this.size();
+    const sizeClass = sizeValue ? { [`fa-${sizeValue}`]: true } : {};
+    return {
+      ...sizeClass,
+      'fa-fw': this.faFw(),
+      'fa-layers': true,
+    };
+  });
+
   private readonly document = inject(DOCUMENT);
-  private readonly renderer = inject(Renderer2);
-  private readonly elementRef = inject(ElementRef);
   private readonly config = inject(FaConfig);
 
   ngOnInit() {
-    this.renderer.addClass(this.elementRef.nativeElement, 'fa-layers');
     ensureCss(this.document, this.config);
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    if ('size' in changes) {
-      if (changes.size.currentValue != null) {
-        this.renderer.addClass(this.elementRef.nativeElement, `fa-${changes.size.currentValue}`);
-      }
-      if (changes.size.previousValue != null) {
-        this.renderer.removeClass(this.elementRef.nativeElement, `fa-${changes.size.previousValue}`);
-      }
-    }
   }
 }
