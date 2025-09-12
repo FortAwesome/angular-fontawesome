88bd605edce62d33d6bb121538b403221e56a3ef
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2025-09-12 20:07:06 +0100

    refactor(tests): update FaLayersTextComponent tests to use TestBed for fixture creation

--- src/lib/layers/layers-text.component.spec.ts
+++ src/lib/layers/layers-text.component.spec.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inputBinding } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { queryByCss } from '../../testing/helpers';
 import { FaLayersComponent } from './layers.component';
@@ -23,15 +23,8 @@ describe('FaLayersTextComponent', () => {
   });
 
   it('should throw an error if text layer is used outside of fa-layers', () => {
-    @Component({
-      selector: 'fa-host',
-      imports: [FaLayersTextComponent],
-      template: `<fa-layers-text content="Test" /> `,
-    })
-    class HostComponent {}
-
-    expect(() => TestBed.createComponent(HostComponent)).toThrow(
-      new Error('FaLayersTextComponent should be used as child of FaLayersComponent only.'),
-    );
+    expect(() =>
+      TestBed.createComponent(FaLayersTextComponent, { bindings: [inputBinding('content', () => 'Test')] }),
+    ).toThrow(new Error('FaLayersTextComponent should be used as child of FaLayersComponent only.'));
   });
 });
